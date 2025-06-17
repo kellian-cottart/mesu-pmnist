a382a9d9299b28312046a3195e70f0db67849e89
kellian <kellian.cottart@gmail.com>
2025-06-17 05:03:22 -0400

    fix: config

--- script.sh
+++ script.sh
@@ -1,19 +1,21 @@
+
+
+python main.py -it 5 -ood fashion -c supp-n-study/mlp-bayesian-mesu-N=180000 -fits -wh -train &&
 python main.py -it 5 -ood fashion -c supp-n-study/mlp-bayesian-mesu-N=300000 -fits -wh -train &&
 python main.py -it 5 -ood fashion -c supp-n-study/mlp-bayesian-mesu-N=600000 -fits -wh -train &&
 python main.py -it 5 -ood fashion -c supp-n-study/mlp-bayesian-mesu-N=1200000 -fits -wh -train &&
 python main.py -it 5 -ood fashion -c supp-n-study/mlp-bayesian-mesu-N=2400000 -fits -wh -train &&
+python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=180000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=300000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=600000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=1200000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=2400000 -wh -fits -train &&
-python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=300000 -wh -fits -train &&
-python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=600000 -wh -fits -train &&
-python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=1200000 -wh -fits -train &&
-python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=256-N=2400000 -wh -fits -train &&
+python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=512-N=180000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=512-N=300000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=512-N=600000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=512-N=1200000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=512-N=2400000 -wh -fits -train &&
+python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=1024-N=180000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=1024-N=300000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=1024-N=600000 -wh -fits -train &&
 python main.py -it 5 -ood fashion -c supp-size-study/mlp-bayesian-mesu-S=1024-N=1200000 -wh -fits -train &&
